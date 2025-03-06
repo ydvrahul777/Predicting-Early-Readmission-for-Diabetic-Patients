fd951e7c567de94ee296f2e264ed5e78eced8325
ydvrahul777 <ry1574862@gmail.com>
2025-03-06 12:52:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,23 +45,25 @@ def main():
     
     with col1:
         st.header('Patient Information')
-        gender = st.selectbox('Gender (0 - Male, 1 - Female)', [0.0, 1.0])
-        age = st.number_input('Age Group (1: 0-10, ..., 10: 90-100)', min_value=1.0, max_value=10.0)
-        admission_type_id = st.selectbox('Admission Type (1 - Emergency, 2 - Urgent, etc.)', [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0])
-        time_in_hospital = st.number_input('Time in Hospital (Days)', min_value=1.0, max_value=30.0)
-        num_lab_procedures = st.number_input('Number of Lab Procedures', min_value=0.0)
-        num_medications = st.number_input('Number of Medications', min_value=0.0)
-        number_inpatient = st.number_input('Number of Inpatient Visits', min_value=0.0)
-        diag_1 = st.number_input('Primary Diagnosis Code')
-        diag_2 = st.number_input('Secondary Diagnosis Code')
-        diag_3 = st.number_input('Additional Diagnosis Code')
-        metformin = st.selectbox('Metformin Use (0 - No, 1 - Yes)', [0.0, 1.0])
-        insulin = st.selectbox('Insulin Use (1 - No, 2 - Up, 3 - Steady)', [1.0, 2.0, 3.0])
-        change = st.selectbox('Change in Medications (0 - No, 1 - Yes)', [0.0, 1.0])
-        diabetesMed = st.selectbox('Diabetes Medication (0 - No, 1 - Yes)', [0.0, 1.0])
-        discharged_to = st.number_input('Discharge Destination Code', min_value=1.0, max_value=30.0)
+        gender = st.checkbox('Male')
+        gender_value = 0 if gender else 1
         
-        input_list = [[gender, age, admission_type_id, time_in_hospital, num_lab_procedures,
+        age = int(st.number_input('Age Group (1: 0-10, ..., 10: 90-100)', min_value=1, max_value=10))
+        admission_type_id = int(st.selectbox('Admission Type (1 - Emergency, 2 - Urgent, etc.)', [1, 2, 3, 4, 5, 6, 7, 8]))
+        time_in_hospital = int(st.number_input('Time in Hospital (Days)', min_value=1, max_value=30))
+        num_lab_procedures = int(st.number_input('Number of Lab Procedures', min_value=0))
+        num_medications = int(st.number_input('Number of Medications', min_value=0))
+        number_inpatient = int(st.number_input('Number of Inpatient Visits', min_value=0))
+        diag_1 = int(st.number_input('Primary Diagnosis Code'))
+        diag_2 = int(st.number_input('Secondary Diagnosis Code'))
+        diag_3 = int(st.number_input('Additional Diagnosis Code'))
+        metformin = int(st.selectbox('Metformin Use (0 - No, 1 - Yes)', [0, 1]))
+        insulin = int(st.selectbox('Insulin Use (1 - No, 2 - Up, 3 - Steady)', [1, 2, 3]))
+        change = int(st.selectbox('Change in Medications (0 - No, 1 - Yes)', [0, 1]))
+        diabetesMed = int(st.selectbox('Diabetes Medication (0 - No, 1 - Yes)', [0, 1]))
+        discharged_to = int(st.number_input('Discharge Destination Code', min_value=1, max_value=30))
+        
+        input_list = [[gender_value, age, admission_type_id, time_in_hospital, num_lab_procedures,
                        num_medications, number_inpatient, diag_1, diag_2, diag_3, metformin,
                        insulin, change, diabetesMed, discharged_to]]
         
